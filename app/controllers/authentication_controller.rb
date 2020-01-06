f27da82429f1e586745f5b0309fdff4ce6f02fc5
github username <github email address>
2020-01-06 02:31:24 -0700

    Created auth.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,20 +1,21 @@
 class AuthenticationController < ApplicationController
 
     def login 
-        user = User.find_by(username: params[:username])
+        user = User.find_by({username: params[:username]})
 
         if(!user)
-            render json: {error: "Invalid username"}
-        end
-
-        if user.authenticate(params[:password])
-            secret_key = Rails.application.secret_key_base[0]
-            token = JWT.encode({
-                user_id: user.id,
-            }, secret_key)
-            render json: {token: token}
-        else
             render status: :unauthorized
+        else
+            if(!user.authenticate(params[:password]))
+                render status: :unauthorized
+            else
+                secret_key = Rails.application.secret_key_base[0]
+                token = JWT.encode({
+                    user_id: user.id,
+                }, secret_key)
+
+                render json: {token: token}
+            end
         end
     end
 
