6fb6a311ce4b725a8e998d270eed08de317c8fa8
github username <github email address>
2020-01-12 14:54:35 -0700

    AAdds item total functioanlity.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,8 +16,7 @@ class ItemsController < ApplicationController
         @item = Item.create(
             name: params[:name],
             description: params[:description],
-            price: params[:price],
-            user_id: params[:user_id]
+            price: params[:price]
         )
         render json: @item
     end
