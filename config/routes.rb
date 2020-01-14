11ae6ea36731f875bd9bec5cbc366684d55ca959
Tatiana Smolin <tatismolin@gmail.com>
2020-01-14 12:20:03 -0700

    Fixes item deletion.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
   resources :items, only: [:index, :show, :create]
   resources :users, only: [:create]
-  resources :user_items, only: [:index, :show, :create]
+  resources :user_items, only: [:index, :show, :create, :destroy]
 
   post "login", to: "authentication#login"
   get "profile", to: "users#profile"
   post "cart", to: "user_items#cart"
   get "show", to: "user_items#show"
-  delete "delete", to: "user_items#delete"
+  # delete "delete/:id", to: "user_items#destroy"
   # get "store", to: "index#store"
 end
--- db/migrate/20200110184250_remove_another_column_from_items.rb
+++ db/migrate/20200110184250_remove_another_column_from_items.rb
@@ -1,4 +1,4 @@
-class RemoveCoanotherColumnFromItems < ActiveRecord::Migration[6.0]
+class RemoveAnotherColumnFromItems < ActiveRecord::Migration[6.0]
   def change
 
     remove_column :items, :quantity, :string
