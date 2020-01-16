a2d7423ea09e2371c69b1da1c26d9a911b7c9183
Tatiana Smolin <tatismolin@gmail.com>
2020-01-16 12:04:38 -0700

    Fixes shopping cart display issues.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,38 +2,31 @@ require "stripe"
 
 class ChargesController < ApplicationController
 
-    skip_before_action :authenticate, except: [:create]
+    skip_before_action :authenticate
 
     def create
-        Stripe.api_key = "pk_test_n25VuFBwG0P8arNmqBOWXehY00B8Jc6bdi"
+        Stripe.api_key = "sk_test_7p9Uk5wzXySyenTCLQMGTuvR00dxls3gHd"
 
-        begin
+        customer = Stripe::Customer.create(
+            email: params[:email],
+            source: params[:id]
+        )
 
-            customer = Stripe::Customer.create(
-                :email => params[:token][:email],
-                :source => params[:token][:id]
-            )
-
-            charge = Stripe::Charge.create({
-                :customer => customer.id,
-                :receipt_email => params[:token][:email]
-                :shipping => {
-                    :name => params[:token][:card][:name],
-                    :address => {
-                        :line1 => params[:token][:card][:address_line1],
-                        :line2 => params[:token][:card][:address_line2],
-                        :city => params[:token][:card][:address_city],
-                        :country => params[:token][:card][:address_country],
-                        :postal_code => params[:token][:card][:address_zip],
-                    }
+        charge = Stripe::Charge.create(
+            customer: customer.id,
+            receipt_email: params[:email],
+            shipping: {
+                name: params[:card][:name],
+                address: {
+                    line1: params[:card][:address_line1],
+                    line2: params[:card][:address_line2],
+                    city: params[:card][:address_city],
+                    country: params[:card][:address_country],
+                    postal_code: params[:card][:address_zip]
                 }
-            }, {
-                :idempotency_key => ip_key
-            })
-
-        rescue Stripe::CardError => e
-            render json: {message: "oops"}, status: :not_acceptable
-        end
+            }, 
+            idempotency_key: ip_key
+        )
     end
 
 end
