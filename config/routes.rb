868e96351f067fd73a962bec4a857f6f2e002222
Tatiana Smolin <tatismolin@gmail.com>
2020-02-12 22:40:31 -0700

    Fixes user_items routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   post "login", to: "authentication#login"
   get "profile", to: "users#profile"
-  # post "cart", to: "user_items#cart"
-  # get "quantities", to: "user_items#quantities"
+  post "create", to: "user_items#create"
+  get "show", to: "user_items#show"
   post "charges", to: "charges#create"
 end
-
